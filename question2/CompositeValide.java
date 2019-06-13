e34406025f37c100c26bff3750973b36da3cc898
Hassane Jalal <jalal.hassane@isae.edu.lb>
2019-06-13 08:29:06 +0300

    Question 2_1

--- question2/CompositeValide.java
+++ question2/CompositeValide.java
@@ -3,18 +3,23 @@ package question2;
 import question1.Contributeur;
 import question1.GroupeDeContributeurs;
 import question1.Visiteur;
+import question1.Cotisant;
+import java.util.List;
 
 public class CompositeValide implements Visiteur<Boolean>{
   // Le solde de chaque contributeur doit être supérieur ou égal à 0 
   // et il n’existe pas de groupe n’ayant pas de contributeurs.
   
   public Boolean visite(Contributeur c){
-    return false; // a compléter;
+      int solde = c.solde();
+      if(solde<0) return false;
+      return true;
   }
   
   public Boolean visite(GroupeDeContributeurs g){
-    boolean res = false;// a compléter
-    // a compléter
+    boolean res = false;
+    List<Cotisant> liste = g.getChildren();
+    if(liste.size()!=0) res = true;
     return res ;
   }
 }
--- question2/SansDoublon.java
+++ question2/SansDoublon.java
@@ -3,15 +3,27 @@ package question2;
 import question1.*;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.List;
 
 public class SansDoublon implements Visiteur<Boolean>{
   public Boolean visite(Contributeur c){
-    return false; // a compléter;
+    int solde = c.solde();
+      if(solde<0) return false;
+      return true;
   }
   
   public Boolean visite(GroupeDeContributeurs g){
-    boolean res = false;// a compléter
-    // a compléter
+    boolean res = true;
+    List<Cotisant> liste = g.getChildren();
+    int i = 0;
+    while(i < liste.size()){
+        Cotisant c1 = liste.get(i);
+        if(i==liste.size()-1) break;
+        Cotisant c2 = liste.get(++i);
+        
+        if(c1.nom().equals(c2.nom())) {res =false; break;}
+        
+    }
     return res ;
   }
   
